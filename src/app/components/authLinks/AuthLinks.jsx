853e45d48f7d68850460ffeb615decccb81815c0
Yosef <yosethio@yahoo.com>
2023-09-06 15:53:41 +0300

    inital commit

---
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import styles from "./authLinks.module.css";
+//import { signOut, useSession } from "next-auth/react";
+
+const AuthLink = () => {
+  const [open, setOpen] = useState(false);
+
+  //const { status } = useSession();
+  const status = "notauthenticated";
+  return (
+    <>
+      {status === "notauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+      ) : (
+        <>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AuthLink;
