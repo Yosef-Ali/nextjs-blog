3cb6c904ea7e7e839a0cff65d74e91d502ccd3f1
Yosef <yosethio@yahoo.com>
2023-09-06 20:47:52 +0300

    category added

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -1,7 +1,112 @@
 import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
-  return <div>CategoryList</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        {/* {data?.map((item) => (
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles[item.slug]}`}
+            key={item._id}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))} */}
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.fashion}`}
+        >
+          <Image
+            src="/fashion.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.food}`}
+        >
+          <Image
+            src="/food.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.travel}`}
+        >
+          <Image
+            src="/travel.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.culture}`}
+        >
+          <Image
+            src="/culture.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.coding}`}
+        >
+          <Image
+            src="/coding.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
